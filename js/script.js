074a004aac3566a6f050cc1ffb7b32012aa9e7a0
Di <yaodi891024@gmail.com>
2016-12-28 21:41:06 -0600

    add restart and reset timer function.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var toe = document.getElementById("toe")
 var start = document.getElementById("start-btn");
 var restart = document.getElementById("restart-btn");
 var reset = document.getElementById("reset-btn");
+var displayNum = document.getElementById("display-number");
+
 
 start.addEventListener("click", startTimer);
 restart.addEventListener("click", restartTimer);
@@ -14,23 +16,36 @@ function startTimer(){
 }
 
 function restartTimer(){
-
+	var timeStop = parseInt(document.getElementById("num").value);
+	timeCount(timeStop);
 }
 
 function resetTimer(){
+	clearAllIntervals();
+	document.getElementById("num").value = 0;
+	displayNum.innerHTML = 0;
+	figure.style.color = "black";
+	toe.style.color = "black";
+	timeNow = 0;
+}
 
+function clearAllIntervals() {
+    // Make sure to clear out all old intervals to restart or start again
+    for (var i = 1; i < 99999; i++) {
+        clearInterval(i);
+    }
 }
 
 function timeCount(timeStop){
-	clearInterval(timer);
+	clearAllIntervals();
 	var timeNow = 0;
-	var displayNum = document.getElementById("display-number");
- 	var timer = setInterval(count, 1000)
+ 	var timer = setInterval(count, 1000);
  	function count(){
  		timeNow++;
  		console.log(timeNow, timeStop)
  		if(timeNow > timeStop){
  			clearInterval(timer);
+ 			timeNow = 0;
  		} else {
  			if(timeNow % 3 === 0  && timeNow %5 === 0){
 	 			figure.style.color = "red";
