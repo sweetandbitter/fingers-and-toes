c15897ea250cb6f370e6e0b64fcdc098711a2ac9
Di <yaodi891024@gmail.com>
2016-12-28 20:41:43 -0600

    create basic html structure.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var start = document.getElementById("start-btn");
+var restart = document.getElementById("restart-btn");
+var reset = document.getElementById("reset-btn");
+
+function startTimer(){
+	
+}
+
+function restartTimer(){
+
+}
+
+function resetTimer(){
+
+}
+
+start.addEventListener("click", startTimer);
+restart.addEventListener("click", restartTimer);
+reset.addEventListener("click", resetTimer);
